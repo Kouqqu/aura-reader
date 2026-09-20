09700f316d682d65c292a08c56803e3069ea3f55
Kouqqu <120759275+Kouqqu@users.noreply.github.com>
2026-09-20 23:39:40 +0300

    release: Aura Reader v1.4.3 (versionCode 54)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aura.reader"
         minSdk = 26
         targetSdk = 35
-        versionCode = 53
-        versionName = "1.4.3-beta.6"
+        versionCode = 54
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
